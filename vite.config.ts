ed8529ef30c98ae4f479cdbd32d667b8a987bf10
ewgeniy <rekoj57@gmail.com>
2024-11-19 09:25:13 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/tatum-hello/',
 	plugins: [preact()],
 	build: {
 		outDir: 'dist',
