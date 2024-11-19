9faa86410a6e38a237a53c79738ccb601c7ad7ef
ewgeniy <rekoj57@gmail.com>
2024-11-19 07:30:32 +0100

    refactor form: add error and loading state

--- src/pages/Home/components/form.tsx
+++ src/pages/Home/components/form.tsx
@@ -9,34 +9,44 @@ import './style.css';
 import { API_KEY_V4 } from "../../../api/const";
 
 function Form() {
-  const [inputValue, setInputValue] = useState(""); // State to hold the input value
-  const [labelText, setLabelText] = useState(""); // State to hold the label text
+    const [address, setAddress] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [balance, setBalance] = useState("");
 
-  const handleButtonClick = async () => {
-      try {
-          const tatum = await TatumSDK.init<Ethereum>({
-              network: Network.ETHEREUM,
-              apiKey: { v4: API_KEY_V4 },
-              verbose: true,
-          });
-          const balance = await tatum.address.getBalance({
-              addresses: [inputValue],
-          });
-          console.log(balance);
-          const balanceData = balance.data.filter(
-              (asset) => asset.asset === "ETH"
-          )[0];
+    const handleButtonClick = async () => {
+        setLoading(true);
+        setError(null);
+        setBalance(null);
+        try {
+            const tatum = await TatumSDK.init<Ethereum>({
+                network: Network.ETHEREUM,
+                apiKey: { v4: API_KEY_V4 },
+            });
+            const bal = await tatum.address.getBalance({
+                addresses: [address],
+            });
 
-          setLabelText(`Balance: ${balanceData.balance}`);
-      } catch (error) {
-          console.error("Error fetching balance:", error);
-          setLabelText("Error while fetching balance");
-      }
-  };
+            if (!bal?.data?.[0]) {
+                const err = bal?.error.message[0];
+                setError(err.toString() || "Unknown error");
+            } else {
+                const balanceData = bal.data.filter(
+                    (asset) => asset.asset === "ETH"
+                )[0];
+                setBalance(balanceData.balance);
+            }
+            await tatum.destroy();
+        } catch (e) {
+            setError(JSON.stringify(e));
+        }
+
+        setLoading(false);
+    }
 
   const onInputChange: JSX.GenericEventHandler<HTMLInputElement> = (e) => {
       if (e.target instanceof HTMLInputElement) {
-          setInputValue(e.target.value.trim());
+          setAddress(e.target.value.trim());
       }
   }
 
@@ -45,16 +55,18 @@ function Form() {
       <p>
         <input
           type="text"
-          value={inputValue}
-          onChange={onInputChange}
+          value={address}
+          onInput={onInputChange}
           class="form__input"
         />
       </p>
       <button onClick={handleButtonClick} class="form__button">
         Click Me
       </button>
-      <p class="form__balance-label">
-        {labelText}
+      <p class={`form__balance-label ${error && "form__balance-label_error"}`}>
+          {loading && "Loading..."}
+          {balance && `Balance: ${balance}`}
+          {error && <>{error}</>}
       </p>
     </div>
   );
