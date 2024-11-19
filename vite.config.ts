968d1c5eedf9a18ef9682ffbf4be955dddac66cc
ewgeniy <rekoj57@gmail.com>
2024-11-19 17:49:33 +0100

    add audit & bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), visualizer()],
   build: {
     outDir: "dist",
   },
