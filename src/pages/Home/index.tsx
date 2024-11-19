31ebc8f027a292b41d2edf3f4e6e672b55e8177e
ewgeniy <rekoj57@gmail.com>
2024-11-19 16:51:45 +0100

    add eslint

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import "./style.css";
 export function Home() {
   return (
     <div class="home">
-      <a href="https://preactjs.com" target="_blank">
+      <a href="https://preactjs.com" target="_blank" rel="noreferrer">
         <img src={preactLogo} alt="Preact logo" height="160" width="160" />
       </a>
       <h1>Tatum Hello</h1>
