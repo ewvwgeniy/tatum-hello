9b4833fdb157dcc6dff2f610d50250f2583563a3
ewgeniy <rekoj57@gmail.com>
2024-11-19 06:17:05 +0100

    refactor form component - add error handling, trim for address input, add .env for api key

---
+++ src/api/const.ts
@@ -0,0 +1,1 @@
+export const API_KEY_V4 = import.meta.env.VITE_API_KEY_V4;
--- src/pages/Home/components/form.tsx
+++ src/pages/Home/components/form.tsx
@@ -6,29 +6,37 @@ import { Network, TatumSDK, Ethereum } from "@tatumio/tatum";
 
 import './style.css';
 
+import { API_KEY_V4 } from "../../../api/const";
+
 function Form() {
   const [inputValue, setInputValue] = useState(""); // State to hold the input value
   const [labelText, setLabelText] = useState(""); // State to hold the label text
 
   const handleButtonClick = async () => {
-    const tatum = await TatumSDK.init<Ethereum>({
-      network: Network.ETHEREUM,
-      apiKey: { v4: "t-65ddbb2bb792d6001be685d9-442dd087e58442acac87f5f9" },
-      verbose: true,
-    });
-    const balance = await tatum.address.getBalance({
-      addresses: [inputValue],
-    });
-    const balanceData = balance.data.filter(
-      (asset) => asset.asset === "ETH"
-    )[0];
+      try {
+          const tatum = await TatumSDK.init<Ethereum>({
+              network: Network.ETHEREUM,
+              apiKey: { v4: API_KEY_V4 },
+              verbose: true,
+          });
+          const balance = await tatum.address.getBalance({
+              addresses: [inputValue],
+          });
+          console.log(balance);
+          const balanceData = balance.data.filter(
+              (asset) => asset.asset === "ETH"
+          )[0];
 
-    setLabelText(`Balance: ${balanceData.balance}`);
+          setLabelText(`Balance: ${balanceData.balance}`);
+      } catch (error) {
+          console.error("Error fetching balance:", error);
+          setLabelText("Error while fetching balance");
+      }
   };
 
   const onInputChange: JSX.GenericEventHandler<HTMLInputElement> = (e) => {
       if (e.target instanceof HTMLInputElement) {
-          setInputValue(e.target.value);
+          setInputValue(e.target.value.trim());
       }
   }
 
