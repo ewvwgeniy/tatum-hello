c9559206f9c52ac441bad01700e509b058542bb1
ewgeniy <rekoj57@gmail.com>
2024-11-19 16:15:22 +0100

    add prettier & prettier check to build script

--- src/pages/Home/components/form.tsx
+++ src/pages/Home/components/form.tsx
@@ -1,71 +1,64 @@
 import { JSX } from "preact";
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
 import { Network, TatumSDK, Ethereum } from "@tatumio/tatum";
 
-import './style.css';
+import "./style.css";
 
 import { API_KEY_V4 } from "../../../api/const";
 
 function Form() {
-    const [address, setAddress] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-    const [balance, setBalance] = useState("");
+  const [address, setAddress] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [balance, setBalance] = useState("");
 
-    const handleButtonClick = async () => {
-        setLoading(true);
-        setError(null);
-        setBalance(null);
-        try {
-            const tatum = await TatumSDK.init<Ethereum>({
-                network: Network.ETHEREUM,
-                apiKey: { v4: API_KEY_V4 },
-            });
-            const bal = await tatum.address.getBalance({
-                addresses: [address],
-            });
+  const handleButtonClick = async () => {
+    setLoading(true);
+    setError(null);
+    setBalance(null);
+    try {
+      const tatum = await TatumSDK.init<Ethereum>({
+        network: Network.ETHEREUM,
+        apiKey: { v4: API_KEY_V4 },
+      });
+      const bal = await tatum.address.getBalance({
+        addresses: [address],
+      });
 
-            if (!bal?.data?.[0]) {
-                const err = bal?.error.message[0];
-                setError(err.toString() || "Unknown error");
-            } else {
-                const balanceData = bal.data.filter(
-                    (asset) => asset.asset === "ETH"
-                )[0];
-                setBalance(balanceData.balance);
-            }
-            await tatum.destroy();
-        } catch (e) {
-            setError(JSON.stringify(e));
-        }
-
-        setLoading(false);
+      if (!bal?.data?.[0]) {
+        const err = bal?.error.message[0];
+        setError(err.toString() || "Unknown error");
+      } else {
+        const balanceData = bal.data.filter((asset) => asset.asset === "ETH")[0];
+        setBalance(balanceData.balance);
+      }
+      await tatum.destroy();
+    } catch (e) {
+      setError(JSON.stringify(e));
     }
 
+    setLoading(false);
+  };
+
   const onInputChange: JSX.GenericEventHandler<HTMLInputElement> = (e) => {
-      if (e.target instanceof HTMLInputElement) {
-          setAddress(e.target.value.trim());
-      }
-  }
+    if (e.target instanceof HTMLInputElement) {
+      setAddress(e.target.value.trim());
+    }
+  };
 
   return (
     <div>
       <p>
-        <input
-          type="text"
-          value={address}
-          onInput={onInputChange}
-          class="form__input"
-        />
+        <input type="text" value={address} onInput={onInputChange} class="form__input" />
       </p>
       <button onClick={handleButtonClick} class="form__button">
         Click Me
       </button>
       <p class={`form__balance-label ${error && "form__balance-label_error"}`}>
-          {loading && "Loading..."}
-          {balance && `Balance: ${balance} ETH`}
-          {error && <>{error}</>}
+        {loading && "Loading..."}
+        {balance && `Balance: ${balance} ETH`}
+        {error && error}
       </p>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
-import preactLogo from '../../assets/tatum.jpeg'
-import Form from './components/form';
-import './style.css';
+import preactLogo from "../../assets/tatum.jpeg";
+import Form from "./components/form";
+import "./style.css";
 
 export function Home() {
-	return (
-		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Tatum Hello</h1>
-			<Form />
-		</div>
-	);
+  return (
+    <div class="home">
+      <a href="https://preactjs.com" target="_blank">
+        <img src={preactLogo} alt="Preact logo" height="160" width="160" />
+      </a>
+      <h1>Tatum Hello</h1>
+      <Form />
+    </div>
+  );
 }
